c7cadd70b9499067672e726f0b4a1c18c14a0fe7
Alexandre Emsenhuber <emsenhuber@lpl.arizona.edu>
2023-04-22 19:54:42 +0200

    Bring some improvements from Python script to the C program
    - Use of timeout() instead of haldelay() for more precise timing
    - Use monotonic timer to provide for precise refresh rate
    - Make commands more like "less"

--- follow.c
+++ follow.c
@@ -16,6 +16,7 @@
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #endif
+#include <sys/param.h> /* For MIN(), MAX() */
 #include <sys/wait.h>
 #ifdef HAVE_FCNTL_H
 #include <fcntl.h>
@@ -125,6 +126,44 @@ char* get_command_output( char** command_args ) {
 	waitpid( pid, NULL, 0 );
 
 	return ret;
+}
+
+/**
+ * Add two timespec's in place
+ */
+void add_timespec( struct timespec* base, const struct timespec* add ) {
+	base->tv_sec += add->tv_sec;
+	base->tv_nsec += add->tv_nsec;
+
+	while ( base->tv_nsec >= 1000000000 ) {
+		base->tv_sec++;
+		base->tv_nsec -= 1000000000;
+	}
+}
+
+/**
+ * Compute the positive difference of two timespec's and return the results in the given 10th power of second
+ */
+long int diff_timespec( const struct timespec* left, const struct timespec* right, const int expo ) {
+	if ( left->tv_sec < right->tv_sec || ( left->tv_sec == right->tv_sec && left->tv_nsec <= right->tv_nsec ) ) return 0;
+
+	long int diff_sec = left->tv_sec - right->tv_sec;
+	long int diff_nsec;
+	if ( left->tv_nsec < right->tv_nsec ) {
+		diff_sec--;
+		diff_nsec = 1000000000 + left->tv_nsec - right->tv_nsec;
+	} else {
+		diff_nsec = left->tv_nsec - right->tv_nsec;
+	}
+
+	if ( expo <= 0 ) {
+		for ( int i = 0; i < -expo; i++ )  diff_sec /= 10;
+		return diff_sec;
+	} else {
+		for ( int i = 0; i < expo; i++ )  diff_sec *= 10;
+		for ( int i = 0; i < 9 - expo; i++ )  diff_nsec /= 10;
+		return diff_sec + diff_nsec;
+	}
 }
 
 /**
@@ -263,15 +302,86 @@ int main( int argc, char** argv ) {
 	keypad( win, 1 );
 
 	int cont = 1;
+	int refresh = 2;
+	struct timespec next_timer = { 0, 0 };
+	struct timespec inter_timer = { interval, 0 };
 
-	int v_bottom = 0;
+	char* title_left = NULL;
+	char* title_right = NULL;
+	char* display_string = NULL;
+	int res_max_height = 0;
+	int res_max_width = 0;
+	size_t lines_alloc = 0;
+	char** lines = NULL;
+	int* lines_len = NULL;
+
 	int v_offset = 0;
 	int h_offset = 0;
+	int v_end = 0;
+	int v_diff = 0;
+	int h_diff = 0;
+	int past = 0;
 
 	while( cont ) {
-		char* title_left = get_title_left( argv[optind] );
-		char* title_right = get_title_right();
-		char* display_string = get_command_output( command_args );
+		if ( refresh ) {
+			if ( refresh == 2 ) {
+				int res = clock_gettime( CLOCK_MONOTONIC, &next_timer );
+				if ( res < 0 ) {
+					perror( "clock_gettime" );
+					exit( EXIT_FAILURE );
+				}
+			}
+			add_timespec( &next_timer, &inter_timer );
+			refresh = 0;
+
+			free( title_left );
+			free( title_right );
+			free( display_string );
+			res_max_height = 0;
+			res_max_width = 0;
+
+			title_left = get_title_left( argv[optind] );
+			title_right = get_title_right();
+			display_string = get_command_output( command_args );
+
+			/* Split the result into lines */
+			if ( display_string != NULL ) {
+				char* line_start = display_string;
+				char* cur_pos = display_string;
+
+				while( 1 ) {
+					char c = *cur_pos;
+					if ( c == '\n' || c == '\0' ) {
+						const size_t line_len = cur_pos - line_start;
+
+						if ( line_len > res_max_width ) {
+							res_max_width = line_len;
+						}
+
+						*cur_pos = '\0';
+
+						if ( c == '\n' || line_len > 0 ) {
+							if ( res_max_height + 1 > lines_alloc ) {
+								lines = realloc( lines, sizeof( char* ) * res_max_height + 1 );
+								lines_len = realloc( lines_len, sizeof( int* ) * res_max_height + 1 );
+								lines_alloc = res_max_height + 1;
+							}
+
+							lines[res_max_height] = line_start;
+							lines_len[res_max_height] = line_len;
+							res_max_height++;
+						}
+
+						if ( c == '\0' ) {
+							break;
+						} else {
+							line_start = cur_pos + 1;
+						}
+					}
+					cur_pos++;
+				}
+			}
+		}
 
 		werase( win );
 		int screen_height = getmaxy( win );
@@ -293,8 +403,6 @@ int main( int argc, char** argv ) {
 				waddnstr( win, title_left, right_start - 4 );
 				waddnstr( win, "...", 3 );
 			}
-
-			free( title_left );
 		}
 
 		if ( title_right != NULL ) {
@@ -305,8 +413,6 @@ int main( int argc, char** argv ) {
 				wmove( win, 0, 0 );
 				waddnstr( win, title_right - right_start, screen_width );
 			}
-
-			free( title_right );
 		}
 
 		wattroff( win, A_REVERSE );
@@ -316,150 +422,131 @@ int main( int argc, char** argv ) {
 		int display_height = screen_height - title_height;
 		int display_width = screen_width;
 
-		/* Size of the result in both directions */
-		int res_max_height = 0;
-		int res_max_width = 0;
+		if ( v_end ) {
+			v_offset = res_max_height > display_height ? res_max_height - display_height : 0;
+		} else if ( v_diff != 0 && past ) {
+			v_offset += v_diff;
+		} else if ( v_diff > 0 ) {
+			v_offset = MAX( v_offset, MIN( v_offset + v_diff, MAX( res_max_height - display_height, 0 ) ) );
+		} else if ( v_diff < 0 ) {
+			v_offset = MIN( v_offset, MAX( v_offset + v_diff, 0 ) );
+		}
 
-		if ( display_string != NULL ) {
-			char* line_start = display_string;
-			char* cur_pos = display_string;
-			char** lines = NULL;
-			int* lines_len = NULL;
-			while( 1 ) {
-				char c = *cur_pos;
-				if ( c == '\n' || c == '\0' ) {
-					const size_t line_len = cur_pos - line_start;
+		if ( h_diff != 0 && past ) {
+			h_offset += h_diff;
+		} else if ( h_diff > 0 ) {
+			h_offset = MAX( h_offset, MIN( h_offset + h_diff, MAX( res_max_width - display_width, 0 ) ) );
+		} else if ( h_diff < 0 ) {
+			h_offset = MIN( h_offset, MAX( h_offset + h_diff, 0 ) );
+		}
 
-					if ( line_len > res_max_width ) {
-						res_max_width = line_len;
-					}
+		if ( display_string != NULL && v_offset > -display_height && v_offset < res_max_height && h_offset > -display_width && h_offset < res_max_width ) {
+			int v_disp_off = MAX( -v_offset, 0 );
+			int v_start = MAX( v_offset, 0 );
+			int h_disp_off = MAX( -h_offset, 0 );
+			int h_start = MAX( h_offset, 0 );
 
-					*cur_pos = '\0';
-
-					if ( c == '\n' || line_len > 0 ) {
-						lines = realloc( lines, sizeof( char* ) * res_max_height + 1 );
-						lines_len = realloc( lines_len, sizeof( int* ) * res_max_height + 1 );
-						lines[res_max_height] = line_start;
-						lines_len[res_max_height] = line_len;
-						res_max_height++;
-					}
-
-					if ( c == '\0' ) {
-						break;
-					} else {
-						line_start = cur_pos + 1;
-					}
-				}
-				cur_pos++;
-			}
-
-			if ( display_height >= res_max_height ) {
-				v_offset = 0;
-			} else if ( v_offset + display_height > res_max_height || v_bottom ) {
-				v_offset = res_max_height - display_height;
-			} else if ( v_offset < 0 ) {
-				v_offset = 0;
-			}
-
-			if ( display_width >= res_max_width ) {
-				h_offset = 0;
-			} else if ( h_offset + display_width > res_max_width ) {
-				h_offset = res_max_width - display_width;
-			} else if ( h_offset < 0 ) {
-				h_offset = 0;
-			}
-
-			const int y_max = res_max_height < display_height ? res_max_height : display_height;
-			for ( int y = 0; y < y_max; y++ ) {
-				const int line_len = lines_len[y + v_offset];
+			const int v_end = MIN( v_offset + display_height, res_max_height ) - v_start;
+			for ( int v = 0; v < v_end; v++ ) {
+				const int line_len = lines_len[v + v_start];
 				if ( line_len <= h_offset ) {
 					continue;
 				}
-				const int x_max = line_len - h_offset < display_width ? line_len - h_offset : display_width;
-				wmove( win, y + title_height, 0 );
-				waddnstr( win, lines[y + v_offset] + h_offset, x_max );
+				const int h_end = MIN( line_len, h_offset + display_width ) - h_start;
+				wmove( win, v_disp_off + v + title_height, h_disp_off );
+				waddnstr( win, lines[v + v_start] + h_start, h_end );
 			}
-
-			free( lines );
-			free( lines_len );
-			free( display_string );
 		} else {
 			perror( NULL );
 		}
 
 		wrefresh( win );
 
-		int begin = 1;
-		while ( 1 ) {
-			if ( begin ) {
-				begin = 0;
-				halfdelay( 10 * interval );
-				nodelay( win, 0 );
-			} else {
-				nodelay( win, 1 );
-				nocbreak();
-			}
+		struct timespec cur_timer = { 0, 0 };
+		int res = clock_gettime( CLOCK_MONOTONIC, &cur_timer );
+		if ( res < 0 ) {
+			perror( "clock_gettime" );
+			exit( EXIT_FAILURE );
+		}
 
-			int c = wgetch( win );
-			int done = 0;
+		wtimeout( win, diff_timespec( &next_timer, &cur_timer, 3 ) );
 
-			switch ( c ) {
-			case 'q':
-				cont = 0;
-				break;
-			case KEY_LEFT:
-				h_offset--;
-				break;
-			case KEY_RIGHT:
-				h_offset++;
-				break;
-			case KEY_UP:
-			case 'k':
-			case 'y':
-				v_bottom = 0;
-				v_offset--;
-				break;
-			case KEY_DOWN:
-			case 'e':
-			case 'j':
-				v_offset++;
-				break;
-			case ' ':
-			case 'f':
-				v_bottom = 0;
-				v_offset += display_height;
-				break;
-			case 'b':
-				v_bottom = 0;
-				v_offset -= display_height;
-				break;
-			case 'd':
-				v_bottom = 0;
-				v_offset += display_height / 2;
-				break;
-			case 'u':
-				v_bottom = 0;
-				v_offset += display_height / 2;
-				break;
-			case 'g':
-				v_bottom = 0;
-				v_offset = 0;
-				break;
-			case 'G':
-				v_bottom = 0;
-				v_offset = res_max_height;
-				break;
-			case 'F':
-				v_bottom = 1;
-				break;
-			default:
-				done = 1;
-				break;
-			}
+		/* Reset these ones */
+		v_diff = 0;
+		h_diff = 0;
+		past = 0;
 
-			if ( done ) {
-				break;
-			}
+		switch ( wgetch( win ) ) {
+		case 'q':
+			cont = 0;
+			break;
+		case 'r':
+		case 'R':
+			refresh = 2;
+			break;
+		case KEY_LEFT:
+			h_diff = -1;
+			break;
+		case KEY_RIGHT:
+			h_diff = 1;
+			break;
+		case KEY_UP:
+		case 'k':
+		case 'y':
+			v_end = 0;
+			v_diff = -1;
+			break;
+		case 'K':
+		case 'Y':
+			v_end = 0;
+			past = 1;
+			v_diff = -1;
+			break;
+		case KEY_DOWN:
+		case 'e':
+		case 'j':
+			v_diff = 1;
+			break;
+		case 'E':
+		case 'J':
+			v_end = 0;
+			past = 1;
+			v_diff = 1;
+			break;
+		case ' ':
+		case 'f':
+			v_end = 0;
+			v_diff = display_height;
+			break;
+		case 'b':
+			v_end = 0;
+			v_diff = -display_height;
+			break;
+		case 'd':
+			v_end = 0;
+			v_diff = display_height / 2;
+			break;
+		case 'u':
+			v_end = 0;
+			v_diff = -display_height / 2;
+			break;
+		case 'g':
+			v_end = 0;
+			v_offset = 0;
+			break;
+		case 'G':
+			v_end = 0;
+			v_offset = 0;
+			v_diff = res_max_height;
+			break;
+		case 'F':
+			v_end = 1;
+			break;
+		case ERR:
+			refresh = 1;
+			break;
+		default:
+			break;
 		}
 	}
 
